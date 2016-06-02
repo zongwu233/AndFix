e4c14b9dd9795a8955bd364187930271c50e883a
houxudong <houxudong@linggan.com>
2016-06-02 12:03:01 +0800

    optimize init cost time

--- src/com/alipay/euler/andfix/AndFixManager.java
+++ src/com/alipay/euler/andfix/AndFixManager.java
@@ -34,209 +34,209 @@ import dalvik.system.DexFile;
 
 /**
  * AndFix Manager
- * 
+ *
  * @author sanping.li@alipay.com
- * 
  */
 public class AndFixManager {
-	private static final String TAG = "AndFixManager";
+    private static final String TAG = "AndFixManager";
 
-	private static final String DIR = "apatch_opt";
+    private static final String DIR = "apatch_opt";
 
-	/**
-	 * context
-	 */
-	private final Context mContext;
+    /**
+     * context
+     */
+    private final Context mContext;
 
-	/**
-	 * classes will be fixed
-	 */
-	private static Map<String, Class<?>> mFixedClass = new ConcurrentHashMap<String, Class<?>>();
+    /**
+     * classes will be fixed
+     */
+    private static Map<String, Class<?>> mFixedClass = new ConcurrentHashMap<String, Class<?>>();
 
-	/**
-	 * whether support AndFix
-	 */
-	private boolean mSupport = false;
+    /**
+     * whether support AndFix
+     */
+    private boolean mSupport = false;
 
-	/**
-	 * security check
-	 */
-	private SecurityChecker mSecurityChecker;
+    /**
+     * security check
+     */
+    private SecurityChecker mSecurityChecker;
 
-	/**
-	 * optimize directory
-	 */
-	private File mOptDir;
+    /**
+     * optimize directory
+     */
+    private File mOptDir;
 
-	public AndFixManager(Context context) {
-		mContext = context;
-		mSupport = Compat.isSupport();
-		if (mSupport) {
-			mSecurityChecker = new SecurityChecker(mContext);
-			mOptDir = new File(mContext.getFilesDir(), DIR);
-			if (!mOptDir.exists() && !mOptDir.mkdirs()) {// make directory fail
-				mSupport = false;
-				Log.e(TAG, "opt dir create error.");
-			} else if (!mOptDir.isDirectory()) {// not directory
-				mOptDir.delete();
-				mSupport = false;
-			}
-		}
-	}
+    public AndFixManager(Context context) {
+        mContext = context;
+        mSupport = Compat.isSupport();
+        if (mSupport) {
+            // mSecurityChecker = new SecurityChecker(mContext); lazy init
+            mOptDir = new File(mContext.getFilesDir(), DIR);
+            if (!mOptDir.exists() && !mOptDir.mkdirs()) {// make directory fail
+                mSupport = false;
+                Log.e(TAG, "opt dir create error.");
+            } else if (!mOptDir.isDirectory()) {// not directory
+                mOptDir.delete();
+                mSupport = false;
+            }
+        }
+    }
 
-	/**
-	 * delete optimize file of patch file
-	 * 
-	 * @param file
-	 *            patch file
-	 */
-	public synchronized void removeOptFile(File file) {
-		File optfile = new File(mOptDir, file.getName());
-		if (optfile.exists() && !optfile.delete()) {
-			Log.e(TAG, optfile.getName() + " delete error.");
-		}
-	}
+    /**
+     * delete optimize file of patch file
+     *
+     * @param file patch file
+     */
+    public synchronized void removeOptFile(File file) {
+        File optfile = new File(mOptDir, file.getName());
+        if (optfile.exists() && !optfile.delete()) {
+            Log.e(TAG, optfile.getName() + " delete error.");
+        }
+    }
 
-	/**
-	 * fix
-	 * 
-	 * @param patchPath
-	 *            patch path
-	 */
-	public synchronized void fix(String patchPath) {
-		fix(new File(patchPath), mContext.getClassLoader(), null);
-	}
+    /**
+     * fix
+     *
+     * @param patchPath patch path
+     */
+    public synchronized void fix(String patchPath) {
+        fix(new File(patchPath), mContext.getClassLoader(), null);
+    }
 
-	/**
-	 * fix
-	 * 
-	 * @param file
-	 *            patch file
-	 * @param classLoader
-	 *            classloader of class that will be fixed
-	 * @param classes
-	 *            classes will be fixed
-	 */
-	public synchronized void fix(File file, ClassLoader classLoader,
-			List<String> classes) {
-		if (!mSupport) {
-			return;
-		}
+    /**
+     * fix
+     *
+     * @param file        patch file
+     * @param classLoader classloader of class that will be fixed
+     * @param classes     classes will be fixed
+     */
+    public synchronized void fix(File file, ClassLoader classLoader,
+                                 List<String> classes) {
+        if (!mSupport) {
+            return;
+        }
 
-		if (!mSecurityChecker.verifyApk(file)) {// security check fail
-			return;
-		}
+        if (!getSecurityChecker().verifyApk(file)) {// security check fail
+            return;
+        }
 
-		try {
-			File optfile = new File(mOptDir, file.getName());
-			boolean saveFingerprint = true;
-			if (optfile.exists()) {
-				// need to verify fingerprint when the optimize file exist,
-				// prevent someone attack on jailbreak device with
-				// Vulnerability-Parasyte.
-				// btw:exaggerated android Vulnerability-Parasyte
-				// http://secauo.com/Exaggerated-Android-Vulnerability-Parasyte.html
-				if (mSecurityChecker.verifyOpt(optfile)) {
-					saveFingerprint = false;
-				} else if (!optfile.delete()) {
-					return;
-				}
-			}
+        try {
+            File optfile = new File(mOptDir, file.getName());
+            boolean saveFingerprint = true;
+            if (optfile.exists()) {
+                // need to verify fingerprint when the optimize file exist,
+                // prevent someone attack on jailbreak device with
+                // Vulnerability-Parasyte.
+                // btw:exaggerated android Vulnerability-Parasyte
+                // http://secauo.com/Exaggerated-Android-Vulnerability-Parasyte.html
+                if (getSecurityChecker().verifyOpt(optfile)) {
+                    saveFingerprint = false;
+                } else if (!optfile.delete()) {
+                    return;
+                }
+            }
 
-			final DexFile dexFile = DexFile.loadDex(file.getAbsolutePath(),
-					optfile.getAbsolutePath(), Context.MODE_PRIVATE);
+            final DexFile dexFile = DexFile.loadDex(file.getAbsolutePath(),
+                    optfile.getAbsolutePath(), Context.MODE_PRIVATE);
 
-			if (saveFingerprint) {
-				mSecurityChecker.saveOptSig(optfile);
-			}
+            if (saveFingerprint) {
+                getSecurityChecker().saveOptSig(optfile);
+            }
 
-			ClassLoader patchClassLoader = new ClassLoader(classLoader) {
-				@Override
-				protected Class<?> findClass(String className)
-						throws ClassNotFoundException {
-					Class<?> clazz = dexFile.loadClass(className, this);
-					if (clazz == null
-							&& className.startsWith("com.alipay.euler.andfix")) {
-						return Class.forName(className);// annotation’s class
-														// not found
-					}
-					if (clazz == null) {
-						throw new ClassNotFoundException(className);
-					}
-					return clazz;
-				}
-			};
-			Enumeration<String> entrys = dexFile.entries();
-			Class<?> clazz = null;
-			while (entrys.hasMoreElements()) {
-				String entry = entrys.nextElement();
-				if (classes != null && !classes.contains(entry)) {
-					continue;// skip, not need fix
-				}
-				clazz = dexFile.loadClass(entry, patchClassLoader);
-				if (clazz != null) {
-					fixClass(clazz, classLoader);
-				}
-			}
-		} catch (Throwable e) {
-			Log.e(TAG, "pacth", e);
-		}
-	}
+            ClassLoader patchClassLoader = new ClassLoader(classLoader) {
+                @Override
+                protected Class<?> findClass(String className)
+                        throws ClassNotFoundException {
+                    Class<?> clazz = dexFile.loadClass(className, this);
+                    if (clazz == null
+                            && className.startsWith("com.alipay.euler.andfix")) {
+                        return Class.forName(className);// annotation’s class
+                        // not found
+                    }
+                    if (clazz == null) {
+                        throw new ClassNotFoundException(className);
+                    }
+                    return clazz;
+                }
+            };
+            Enumeration<String> entrys = dexFile.entries();
+            Class<?> clazz = null;
+            while (entrys.hasMoreElements()) {
+                String entry = entrys.nextElement();
+                if (classes != null && !classes.contains(entry)) {
+                    continue;// skip, not need fix
+                }
+                clazz = dexFile.loadClass(entry, patchClassLoader);
+                if (clazz != null) {
+                    fixClass(clazz, classLoader);
+                }
+            }
+        } catch (Throwable e) {
+            Log.e(TAG, "pacth", e);
+        }
+    }
 
-	/**
-	 * fix class
-	 * 
-	 * @param clazz
-	 *            class
-	 */
-	private void fixClass(Class<?> clazz, ClassLoader classLoader) {
-		Method[] methods = clazz.getDeclaredMethods();
-		MethodReplace methodReplace;
-		String clz;
-		String meth;
-		for (Method method : methods) {
-			methodReplace = method.getAnnotation(MethodReplace.class);
-			if (methodReplace == null)
-				continue;
-			clz = methodReplace.clazz();
-			meth = methodReplace.method();
-			if (!isEmpty(clz) && !isEmpty(meth)) {
-				replaceMethod(classLoader, clz, meth, method);
-			}
-		}
-	}
+    /**
+     * fix class
+     *
+     * @param clazz class
+     */
+    private void fixClass(Class<?> clazz, ClassLoader classLoader) {
+        Method[] methods = clazz.getDeclaredMethods();
+        MethodReplace methodReplace;
+        String clz;
+        String meth;
+        for (Method method : methods) {
+            methodReplace = method.getAnnotation(MethodReplace.class);
+            if (methodReplace == null)
+                continue;
+            clz = methodReplace.clazz();
+            meth = methodReplace.method();
+            if (!isEmpty(clz) && !isEmpty(meth)) {
+                replaceMethod(classLoader, clz, meth, method);
+            }
+        }
+    }
 
-	/**
-	 * replace method
-	 * 
-	 * @param classLoader classloader
-	 * @param clz class
-	 * @param meth name of target method 
-	 * @param method source method
-	 */
-	private void replaceMethod(ClassLoader classLoader, String clz,
-			String meth, Method method) {
-		try {
-			String key = clz + "@" + classLoader.toString();
-			Class<?> clazz = mFixedClass.get(key);
-			if (clazz == null) {// class not load
-				Class<?> clzz = classLoader.loadClass(clz);
-				// initialize target class
-				clazz = AndFix.initTargetClass(clzz);
-			}
-			if (clazz != null) {// initialize class OK
-				mFixedClass.put(key, clazz);
-				Method src = clazz.getDeclaredMethod(meth,
-						method.getParameterTypes());
-				AndFix.addReplaceMethod(src, method);
-			}
-		} catch (Exception e) {
-			Log.e(TAG, "replaceMethod", e);
-		}
-	}
+    /**
+     * replace method
+     *
+     * @param classLoader classloader
+     * @param clz         class
+     * @param meth        name of target method
+     * @param method      source method
+     */
+    private void replaceMethod(ClassLoader classLoader, String clz,
+                               String meth, Method method) {
+        try {
+            String key = clz + "@" + classLoader.toString();
+            Class<?> clazz = mFixedClass.get(key);
+            if (clazz == null) {// class not load
+                Class<?> clzz = classLoader.loadClass(clz);
+                // initialize target class
+                clazz = AndFix.initTargetClass(clzz);
+            }
+            if (clazz != null) {// initialize class OK
+                mFixedClass.put(key, clazz);
+                Method src = clazz.getDeclaredMethod(meth,
+                        method.getParameterTypes());
+                AndFix.addReplaceMethod(src, method);
+            }
+        } catch (Exception e) {
+            Log.e(TAG, "replaceMethod", e);
+        }
+    }
 
-	private static boolean isEmpty(String string) {
-		return string == null || string.length() <= 0;
-	}
+    public synchronized SecurityChecker getSecurityChecker() {
+        if (mSecurityChecker == null) {
+            mSecurityChecker = new SecurityChecker(mContext);
+        }
+        return mSecurityChecker;
+    }
+
+    private static boolean isEmpty(String string) {
+        return string == null || string.length() <= 0;
+    }
 
 }
